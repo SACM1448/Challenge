04d286f6d3fbe6c9dd950d3c90caab382de28087
SACM <sergiomoya757mail.com>
2024-08-15 15:28:38 -0500

    Modulo JS

--- js/app.js
+++ js/app.js
@@ -1,50 +1,84 @@
+"use strict";
 function mostrarMensaje(texto) {
-    const mensaje = document.getElementById("mensaje");
-    mensaje.textContent = texto;
+  const mensajeI = document.getElementById("mensajeI");
+  const mensajeF = document.getElementById("mensajeF");
+  mensajeF.textContent = texto;
+  mensajeF.style.fontSize="1.5rem";
+  mensajeF.style.color="#495057";
+  mensajeF.style.marginTop="auto";
+  mensajeI.style.display="none";
+}
+function capturar_texto_dividirlo() {
+  const cadena = document.getElementById("texto_encrip").value;
+  const regex = /^[a-z\s?!*%@#&]+$/
+  if (!regex.test(cadena)) {
+    mostrarMensaje("El mensaje solo puede contener letras minúsculas y sin acentos.");
   }
-  mostrarMensaje("Ingresa el texto que desees encriptar o desencriptar");
-  
-  function capturarTexto() {
-    // Obtener el valor del texto a encriptar
-    const cadena = document.getElementById("texto_encrip").value;
-    // Dividir el texto en palabras
-    const cadenaDividida = cadena.split(/\s+/);
-    // Definir el diccionario de encriptación
-    const llaves = {
-      e: "enter",
-      i: "imes",
-      a: "ai",
-      o: "ober",
-      u: "ufat",
-    };
-    // Función para encriptar una palabra utilizando el diccionario
-    function usarLlaves(palabra) {
-      return palabra
-        .split("")
-        .map((letra) => llaves[letra] || letra)
-        .join("");
-    }
-    
-    // Encriptar cada palabra en la cadena
-    const palabrasEncriptadas = cadenaDividida.map(usarLlaves);
-    const cadenaCompleta = palabrasEncriptadas.join(" ");
-    
-    mostrarMensaje("Mensaje encriptado: " + cadenaCompleta);
-    
-    // Ocultar el mensaje de no encontrado y la imagen
-    const mensajeNoEncontrado = document.getElementById("mensaje_no_encotrado");
-    mensajeNoEncontrado.style.display = "none";
-    
-    const img = document.getElementById("img");
-    img.style.display = "none";
-    
-    // Mostrar el botón de copiar y ajustar su estilo según el tamaño de la pantalla
-    const btn = document.getElementById("btn_cop");
-    btn.style.display = "flex";
-    
-    const mediaQuery = window.matchMedia("(min-width: 1025px)");
-    if (mediaQuery.matches) {
-      btn.style.marginTop = "110%";
-    }
+  else{
+    return cadena.split(/\s+/);    
   }
+}
+
+function usar_llaves(diccionario, palabra) {
+  // Recorre cada clave en el diccionario
+  for (const [clave, valor] of Object.entries(diccionario)) {
+    // Crea una expresión regular para buscar todas las ocurrencias de la clave
+    const regex = new RegExp(clave, "g");
+    // Reemplaza las ocurrencias de la clave en la palabra por el valor correspondiente
+    palabra = palabra.replace(regex, valor);
+    
+  }
+  return palabra;
+}
+
+function encriptar_texto() {
+
+  const llaves_encrip = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat",
+  };
+
+  const palabrasEncriptadas = capturar_texto_dividirlo().map((palabra) =>
+    usar_llaves(llaves_encrip, palabra)
+  );
+  const cadenaCompleta = palabrasEncriptadas.join(" ");
+
+  mostrarMensaje("Mensaje encriptado: " + cadenaCompleta);
+  mostrar_btnCop_ocultar_p_img();
+}
+
+function desencriptar_texto() {
+
+  const llaves_desencrip = {
+    enter: "e",
+    imes: "i",
+    ai: "a",
+    ober: "o",
+    ufat: "u",
+  };
   
+  const palabrasDesencriptadas = capturar_texto_dividirlo().map((palabra) =>
+    usar_llaves(llaves_desencrip, palabra)
+  );
+  const cadenaCompleta = palabrasDesencriptadas.join(" ");
+
+  mostrarMensaje("Mensaje desencriptado: " + cadenaCompleta);
+  mostrar_btnCop_ocultar_p_img();
+}
+
+function mostrar_btnCop_ocultar_p_img() {
+  const mensajeNoEncontrado = document.getElementById("mensaje_no_encotrado");
+  mensajeNoEncontrado.style.display = "none";
+
+  const img = document.getElementById("img");
+  img.style.display = "none";
+
+  const btn = document.getElementById("btn_cop");
+  btn.style.display = "flex";
+}
+
+document.getElementById("encriptar").addEventListener("click", encriptar_texto);
+document.getElementById("desencriptar").addEventListener("click", desencriptar_texto);
